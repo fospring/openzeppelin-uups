5cf69a30a363d21176501addf1839bbb329f8f4b
Spring Chiu <fospring@outlook.com>
2024-09-04 00:11:51 +0800

    feat: init commit

---
+++ test/MyToken.test.js
@@ -0,0 +1,10 @@
+// test/MyToken.test.js
+
+const { ethers, upgrades } = require('hardhat');
+
+describe('MyToken', function () {
+  it('deploys', async function () {
+    const MyTokenV1 = await ethers.getContractFactory('MyTokenV1');
+    await upgrades.deployProxy(MyTokenV1, { kind: 'uups' });
+  });
+});
